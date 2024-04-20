99f791b88b6bc3156b31f9b6a3b271c2c5ed7895
Cristian Rojas <crissrc811@gmail.com>
2024-04-20 23:49:14 +0200

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,82 @@
+<script>
+  let displayValue = '';
+  let result = '';
+
+  function addToDisplay(value) {
+    displayValue += value;
+  }
+
+  function clearDisplay() {
+    displayValue = '';
+    result = '';
+  }
+
+  function calculateResult() {
+    try {
+      result = eval(displayValue);
+    } catch (error) {
+      result = 'Error';
+    }
+  }
+</script>
+
+<svelte:head>
+  <title>Calculator</title>
+  <meta name="description" content="Simple calculator built with Svelte" />
+</svelte:head>
+
+<section class="calculator">
+  <input type="text" bind:value={displayValue} readonly />
+  <div class="buttons">
+    <button on:click={addToDisplay.bind(null, '7')}>7</button>
+    <button on:click={addToDisplay.bind(null, '8')}>8</button>
+    <button on:click={addToDisplay.bind(null, '9')}>9</button>
+    <button on:click={addToDisplay.bind(null, '+')}>+</button>
+    <button on:click={addToDisplay.bind(null, '4')}>4</button>
+    <button on:click={addToDisplay.bind(null, '5')}>5</button>
+    <button on:click={addToDisplay.bind(null, '6')}>6</button>
+    <button on:click={addToDisplay.bind(null, '-')}>-</button>
+    <button on:click={addToDisplay.bind(null, '1')}>1</button>
+    <button on:click={addToDisplay.bind(null, '2')}>2</button>
+    <button on:click={addToDisplay.bind(null, '3')}>3</button>
+    <button on:click={addToDisplay.bind(null, '*')}>*</button>
+    <button on:click={addToDisplay.bind(null, '0')}>0</button>
+    <button on:click={addToDisplay.bind(null, '.')}>.</button>
+    <button on:click={calculateResult}>=</button>
+    <button on:click={addToDisplay.bind(null, '/')} >/</button>
+    <button on:click={clearDisplay}>C</button>
+  </div>
+  <p>Result: {result}</p>
+</section>
+
+<style>
+  .calculator {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-top: 50px;
+  }
+
+  input {
+    width: 200px;
+    padding: 10px;
+    margin-bottom: 10px;
+  }
+
+  .buttons {
+    display: grid;
+    grid-template-columns: repeat(4, 50px);
+    grid-gap: 5px;
+  }
+
+  button {
+    padding: 10px;
+    font-size: 16px;
+    cursor: pointer;
+  }
+
+  p {
+    margin-top: 20px;
+  }
+</style>
---
+++ src/routes/timer/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+  let milliseconds = 0;
+  let active = false;
+  let intervalId;
+
+  const startTimer = () => {
+    if (!active) {
+      active = true
+      intervalId = setInterval(() => {
+        milliseconds += 10;
+      }, 10);
+    }
+  }
+
+  const stopTimer = () => {
+    if (active) {
+      clearInterval(intervalId);
+      active = false
+    }
+  }
+
+  const resetTimer = () => {
+    milliseconds = 0;
+  }
+</script>
+
+<svelte:head>
+	<title>Ejemplo 2</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+  <p>Tiempo transcurrido(MS): {milliseconds}</p>
+  <button on:click={startTimer}>Iniciar</button>
+  <button on:click={stopTimer}>Parar</button>
+  <button on:click={resetTimer}>Reiniciar</button>
+</div>
